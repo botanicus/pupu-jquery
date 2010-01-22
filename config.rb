308edbb14a3fc2fd7694dde5bef35fa11487e08c
Jakub Stastny aka Botanicus <knava.bestvinensis@gmail.com>
2010-01-22 13:15:39 +0000

    Edited config.rb

--- config.rb
+++ config.rb
@@ -1,12 +1,3 @@
-# dependencies of the pupu
+# encoding: utf-8
 
-# load javascripts
 javascript "jquery"
-
-# load stylesheets
-
-# parameters of plugin
-# it will be blank in most cases
-#parameter :request, :optional => ["local", "ajax"] do |type|
-#  javascript "lib/autocompleter.#{type}"
-#end
