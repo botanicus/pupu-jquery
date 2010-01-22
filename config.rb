0100b77ce26eb4b338c87ed16dade2f0d2f311df
Jakub Stastny aka Botanicus <knava.bestvinensis@gmail.com>
2010-01-22 13:22:31 +0000

    Added jquery & jquery.min, added parameter minified for including jquery.min (defaults to true)

--- config.rb
+++ config.rb
@@ -1,3 +1,7 @@
 # encoding: utf-8
 
-javascript "jquery"
+# pupu :jquery
+# pupu :jquery, minified: false
+parameter :minified, :optional => [true, false] do |minified = true|
+  javascript minified ? "jquery-1.4.min" : "jquery-1.4"
+end
